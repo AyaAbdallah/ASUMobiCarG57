632152c24a110614991fdadd5f40b686960ad39b
sara <36546098+SaraSafwat@users.noreply.github.com>
2018-07-06 14:34:45 -0700

    Add files via upload

---
+++ phase3/char.ino
@@ -0,0 +1,31 @@
+#include <SoftwareSerial.h>
+SoftwareSerial myserial (0,1);
+String x ;
+char z = 'w'  ;
+float y    ;   
+
+void setup() {
+pinMode ( 13 , OUTPUT );
+myserial.begin(9600);
+Serial.begin(9600);
+
+}
+
+void loop() {
+if (myserial.available() )
+{
+  
+  z = myserial.read() ;
+  Serial.write(myserial.read());
+ 
+
+if ( z != ' ' )
+      x = x + z; 
+      else{
+ y = x.toFloat();
+     Serial.print (y);
+     x = "";
+      }
+                
+}
+}
